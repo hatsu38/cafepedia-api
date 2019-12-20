f2d46ed2a79627772235b73081569243dad7e3c3
hatsu38 <hajiwata0308@gmail.com>
2019-12-20 21:14:47 +0900

    :+1:管理画面から混雑情報タイプの登録を可能にする

--- app/admin/main_shops.rb
+++ app/admin/main_shops.rb
@@ -2,18 +2,4 @@
 
 ActiveAdmin.register MainShop do
   permit_params :name, :image
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :image
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :image]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 end
