f50e5262c0edcfe24ee5619c149c386a10793195
hatsu38 <hajiwata0308@gmail.com>
2019-12-21 12:53:18 +0900

    :recycle:投稿削除のリファクタリング

--- app/controllers/api/shop_congrestion_infos_controller.rb
+++ app/controllers/api/shop_congrestion_infos_controller.rb
@@ -4,7 +4,7 @@ module Api
   class ShopCongrestionInfosController < ApplicationController
     def destroy
       shop_congrestion_info = ShopCongrestionInfo.find_by(id: params[:id])
-      if shop_congrestion_info && shop_congrestion_info.destroy
+      if shop_congrestion_info&.destroy
         render json: { status: 'ok' }
       else
         Rails.logger.error("id: #{shop_congrestion_info} の混雑情報の投稿の削除に失敗しました")
