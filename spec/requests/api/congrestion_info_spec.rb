ae7f13d77c865390d8da155b8edb720aa822c333
hatsu38 <hajiwata0308@gmail.com>
2019-12-23 08:56:06 +0900

    :recycle:Add Post,Delete Method RSpec

--- spec/requests/api/congrestion_info_spec.rb
+++ spec/requests/api/congrestion_info_spec.rb
@@ -1,21 +1,49 @@
 require 'rails_helper'
 
 describe 'Api::CongrestionInfosController', type: :request do
-  describe "post message" do
-    it "should not increment the Message count" do
-      expect do
-        post api_shop_congrestion_infos_path,
-          params: { shop_id: 1,
-                    congrestion_info_id: 1
-                  }
-      end.to change(ShopCongrestionInfos, :count).by(1)
+  describe 'Post Shop Congrestion Info' do
+    describe 'Work' do
+      let!(:shop) { create(:shop) }
+      let!(:congrestion_info) { create(:congrestion_info) }
+      let(:params) do
+        {
+          shop_id: shop.id,
+          congrestion_infos: { id: congrestion_info.id }
+        }
+      end
+
+      it 'increments the Congrestion count' do
+        expect do
+          post api_shop_congrestion_infos_path(params)
+        end.to change(ShopCongrestionInfo, :count).by(1)
+      end
+
+      it 'Success Response' do
+        post api_shop_congrestion_infos_path(params)
+        json = JSON.parse(response.body)
+        expect(json['status']).to eq('ok')
+      end
     end
-    it "should not increment the Message count" do
-      post api_shop_congrestion_infos_path,
-        params: { shop_id: 1,
-                  congrestion_info_id: 1
-                }
-      expect(json[:status]).to eq('ok')
+
+    describe 'Not Work' do
+      let(:params) do
+        {
+          shop_id: 1,
+          congrestion_infos: { id: 1 }
+        }
+      end
+
+      it 'does not increment the Congrestion count' do
+        expect do
+          post api_shop_congrestion_infos_path(params)
+        end.to change(ShopCongrestionInfo, :count).by(0)
+      end
+
+      it 'Error Response' do
+        post api_shop_congrestion_infos_path(params)
+        json = JSON.parse(response.body)
+        expect(json['status']).to eq('error')
+      end
     end
   end
 end
