5a0c9ebb15d423c0cd781f5e7e7a36c746901b58
hatsu38 <hajiwata0308@gmail.com>
2020-08-19 18:30:59 +0900

    Create City Show Json

--- app/controllers/api/v1/prefectures/cities_controller.rb
+++ app/controllers/api/v1/prefectures/cities_controller.rb
@@ -8,8 +8,9 @@ module Api::V1::Prefectures
     end
 
     def show
-      @prefecture = Prefecture.find_by(name_e: params[:name_e])
-      @shops = @prefecture.shops.page(params[:page]).per(params[:per] || PER)
+      @prefecture = Prefecture.find_by(name_e: params[:prefecture_name_e])
+      @city = City.where(prefecture_id: @prefecture.id).find_by(city_code: params[:city_code])
+      @shops = @city.shops.eager_load(:main_shop).page(params[:page]).per(params[:per] || PER)
     end
   end
 end
