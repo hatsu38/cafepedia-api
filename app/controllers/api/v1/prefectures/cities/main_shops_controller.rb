eda0747692fc3cf8e203b60444a3c1ed135395ac
hatsu38 <hajiwata0308@gmail.com>
2020-08-22 13:49:27 +0900

    Add Before Action

--- app/controllers/api/v1/prefectures/cities/main_shops_controller.rb
+++ app/controllers/api/v1/prefectures/cities/main_shops_controller.rb
@@ -2,19 +2,24 @@
 
 module Api::V1::Prefectures::Cities
   class MainShopsController < ApplicationController
+    before_action :set_address
+
     PER = 20
     def index
-      @prefecture = Prefecture.find_by(name_e: params[:prefecture_name_e])
-      @city = @prefecture.cities.find_by(city_code: params[:city_code])
       @shops = @city.shops.open.eager_load(:main_shop).page(params[:page]).per(params[:per] || PER)
       @main_shops = MainShop.eager_load(:shops)
     end
 
     def show
-      @prefecture = Prefecture.find_by(name_e: params[:prefecture_name_e])
-      @city = @prefecture.cities.find_by(city_code: params[:city_code])
       @main_shop = MainShop.find_by(eng_name: params[:eng_name])
       @shops = @city.shops.open.where(main_shop_id: @main_shop.id).page(params[:page]).per(params[:per] || PER)
+    end
+
+    private
+
+    def set_address
+      @prefecture = Prefecture.find_by(name_e: params[:prefecture_name_e])
+      @city = @prefecture.cities.find_by(city_code: params[:city_city_code])
     end
   end
 end
--- app/controllers/api/v1/prefectures/cities_controller.rb
+++ app/controllers/api/v1/prefectures/cities_controller.rb
@@ -1,16 +1,22 @@
 module Api::V1::Prefectures
   class CitiesController < ApplicationController
+    before_action :set_pfefecture
+
     PER = 10
     def index
-      @prefecture = Prefecture.find_by(name_e: params[:prefecture_name_e])
       @cities = @prefecture.cities.eager_load(:shops)
       @shops = @prefecture.shops.open.eager_load(:main_shop).page(params[:page]).per(params[:per] || PER)
     end
 
     def show
-      @prefecture = Prefecture.find_by(name_e: params[:prefecture_name_e])
-      @city = @pref.cities.find_by(city_code: params[:city_code])
+      @city = @prefecture.cities.find_by(city_code: params[:city_code])
       @shops = @city.shops.open.eager_load(:main_shop).page(params[:page]).per(params[:per] || PER)
+    end
+
+    private
+
+    def set_pfefecture
+      @prefecture = Prefecture.find_by(name_e: params[:prefecture_name_e])
     end
   end
 end
