7a346bef5434011b93122e13a273a4cbb9ca9688
hatsu38 <hajiwata0308@gmail.com>
2020-08-27 02:13:33 +0900

    Delete Comment Out In db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,20 @@
 require 'csv'
 
-Dir.glob(Rails.root.join("db/seeds/create/cities.rb")).sort.each do |file|
+Dir.glob(Rails.root.join("db/seeds/create/*.rb")).sort.each do |file|
   require file
 end
 
-# Dir.glob(Rails.root.join("db/seeds/create/*.rb")).sort.each do |file|
-#   require file
-# end
+Dir.glob(Rails.root.join("db/seeds/fix/*.rb")).sort.each do |file|
+  require file
+end
 
-# Dir.glob(Rails.root.join("db/seeds/fix/*.rb")).sort.each do |file|
-#   require file
-# end
+Dir.glob(Rails.root.join("db/seeds/set_reference/*.rb")).sort.each do |file|
+  require file
+end
 
-# Dir.glob(Rails.root.join("db/seeds/set_reference/*.rb")).sort.each do |file|
-#   require file
-# end
-
-# if AdminUser.find_by(email: Rails.application.credentials.admin[:email]).nil?
-#   AdminUser.create!(
-#     email: Rails.application.credentials.admin[:email],
-#     password: Rails.application.credentials.admin[:password]
-#   )
-# end
+if AdminUser.find_by(email: Rails.application.credentials.admin[:email]).nil?
+  AdminUser.create!(
+    email: Rails.application.credentials.admin[:email],
+    password: Rails.application.credentials.admin[:password]
+  )
+end
