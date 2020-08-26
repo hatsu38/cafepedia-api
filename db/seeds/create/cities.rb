7b66d2a503b43edf5e1cd31a59ee72a091bd92f8
hatsu38 <hajiwata0308@gmail.com>
2020-08-27 02:12:29 +0900

    Delete Postalcodes CSV

--- db/seeds/create/cities.rb
+++ db/seeds/create/cities.rb
@@ -1,7 +1,9 @@
-datas = CSV.read('./db/datas/postalcodes/postalcodes.csv')
+require 'open-uri'
+
+csv = URI.parse('https://raw.githubusercontent.com/geolonia/japanese-addresses/master/data/latest.csv').open { |f| f.read }
 
 # 市区町村コードでユニークする
-cities_by_uniqed_city_code = datas[1..-1].uniq { |data| data[4] }
+cities_by_uniqed_city_code = CSV.parse(csv)[1..-1].uniq { |data| data[4] }
 
 # 必要なデータのIndexをとる
 city_code_index = datas[0].find_index("市区町村コード")
@@ -12,10 +14,10 @@ cities = []
 cities_by_uniqed_city_code.each do |data|
   city = City.find_by(code: data[city_code_index])
 
-  next if city && city.name == data[city_name_index]
+  next if city && (city.name == data[city_name_index] || city.name.gsub(/ケ/, 'ヶ') == data[city_name_index])
 
   if city
-    city.name = data[city_name_index]
+    city.name = data[city_name_index].gsub(/ケ/, 'ヶ')
     city.prefecture_id = data[prefecutre_code_index].to_i
     cities << city
     next
--- db/seeds/create/stations.rb
+++ db/seeds/create/stations.rb
@@ -2,8 +2,9 @@ require 'csv'
 
 CSV.foreach('./db/datas/stations/station.csv', headers: true) do |data|
   kanji_name = data['kanji'].include?('(') ? data['kanji'].sub!(/\(.*/m, '') : data['kanji']
+  fixed_kanji_name = kanji_name.gsub(/ケ/, 'ヶ')
   Station.find_or_create_by!(
-    kanji_name: kanji_name,
+    kanji_name: fixed_kanji_name,
     kana_name: data['kana']
   )
 rescue StandardError
