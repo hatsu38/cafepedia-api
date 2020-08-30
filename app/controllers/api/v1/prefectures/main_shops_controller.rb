4f4a2f8871c063c06e2b9c4a94135942c2ae7076
hatsu38 <hajiwata0308@gmail.com>
2020-08-30 11:22:59 +0900

    Bug Fix

--- app/controllers/api/v1/prefectures/main_shops_controller.rb
+++ app/controllers/api/v1/prefectures/main_shops_controller.rb
@@ -6,7 +6,7 @@ module Api
         def show
           @prefecture = Prefecture.find_by(name_e: params[:prefecture_name_e])
           @main_shop = MainShop.find_by(eng_name: params[:eng_name])
-          @stations = @prefecturestations.popular.preload(:city)
+          @stations = @prefecture.stations.popular.preload(:city)
           @cities = @prefecture.cities.popular
           @shops = @main_shop.shops
                              .where(prefecture_id: @prefecture.id)
