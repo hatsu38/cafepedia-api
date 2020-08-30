f2a94dec778a8f6ebd756affd89fc964dfa9326a
hatsu38 <hajiwata0308@gmail.com>
2020-08-30 12:10:51 +0900

    Add MainShops#index

--- app/controllers/api/v1/main_shops_controller.rb
+++ app/controllers/api/v1/main_shops_controller.rb
@@ -2,6 +2,10 @@ module Api
   module V1
     class MainShopsController < ApplicationController
       PER = 20
+      def index
+        @main_shops = MainShop.all.eager_load(:shops)
+      end
+
       def show
         @main_shop = MainShop.find_by(eng_name: params[:eng_name])
         @prefectures = Prefecture.popular
---
+++ app/views/api/v1/main_shops/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.main_shops @main_shops, partial: "api/v1/commons/main_shop", as: :main_shop
