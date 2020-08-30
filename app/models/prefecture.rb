a850218ad4bb05da2f6ff37046389a1f2270576d
hatsu38 <hajiwata0308@gmail.com>
2020-08-30 11:59:34 +0900

    Add Popular Method In Prefecture Model

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -6,5 +6,10 @@ class Prefecture < ActiveYaml::Base
   has_many :shops, dependent: :destroy
   has_many :cities, dependent: :destroy
   has_many :stations, dependent: :destroy
+
+  def self.popular(limit: 10)
+    popular_prefecture_ids = Shop.group(:prefecture_id).count.sort_by{ |_, v| -v }[0, limit].map(&:first)
+    Prefecture.where(id: popular_prefecture_ids)
+  end
 end
 
