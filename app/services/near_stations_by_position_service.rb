8cabafb0b51cfd4fdc50d6cc1a98b3a363800353
hatsu38 <hajiwata0308@gmail.com>
2020-09-14 02:11:36 +0900

    緯度経度から近くの駅と市区町村を取得

---
+++ app/services/near_stations_by_position_service.rb
@@ -0,0 +1,29 @@
+class NearStationsByPositionService
+
+  def execute(latitude, longitude)
+    stations = get_near_stations_json(latitude, longitude)
+    return nil unless stations
+
+    station_names = stations.map{|station| station["name"]}
+    station_names += station_names.map{|station| station + "駅" }
+    Station.where(kanji_name: station_names)
+  end
+
+  private
+
+  def get_near_stations_json(latitude, longitude)
+    count = 0
+    uri = URI("https://express.heartrails.com/api/json")
+    params = { method: "getStations", x: longitude, y: latitude }
+    uri.query = URI.encode_www_form(params)
+    begin
+      response = Net::HTTP.get_response(uri)
+      response.is_a?(Net::HTTPSuccess) ? JSON.parse(response.body)["response"]["station"] : nil
+    rescue => error
+      return nil if count > 3
+      sleep(1)
+      count += 1
+      get_near_stations_json(latitude, longitude)
+    end
+  end
+end
