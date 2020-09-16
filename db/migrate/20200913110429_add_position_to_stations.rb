8bb85053d64d6f9c8a51f775ad9117be771ef93a
hatsu38 <hajiwata0308@gmail.com>
2020-09-16 16:57:55 +0900

    delete migrate file lat lng station

--- db/migrate/20200913110429_add_position_to_stations.rb
+++
@@ -1,6 +0,0 @@
-class AddPositionToStations < ActiveRecord::Migration[6.0]
-  def change
-    add_column :stations, :latitude, :decimal, precision: 10, scale: 7, null: false, default: 0, comment: "緯度"
-    add_column :stations, :longitude, :decimal, precision: 9, scale: 7, null: false, default: 0, comment: "経度"
-  end
-end
