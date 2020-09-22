25c8395a7c02ee5323d22bc10c003df1bc4c4448
hatsu38 <hajiwata0308@gmail.com>
2020-09-22 19:02:07 +0900

    rubocop fix

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class BaseController < ApplicationController
-
       if Rails.env.production? || Rails.env.staging?
         rescue_from StandardError, with: :rescue_500
         rescue_from ActionController::RoutingError, with: :rescue_404
@@ -11,24 +10,24 @@ module Api
         rescue_from ActiveHash::RecordNotFound, with: :rescue_404
       end
 
-      def rescue_404(e = nil)
+      def rescue_404(error = nil)
         Rails.logger.warn(
           "message: 404 NotFound #{request.url},
-          #{e&.message},
-          #{e&.class}"
+          #{error&.message},
+          #{error&.class}"
         )
         Raven.extra_context(params: params&.to_unsafe_h, url: request.url)
-        render json: { message: "not found", status: 404 }
+        render json: { message: 'not found', status: 404 }
       end
 
-      def rescue_500(e = nil)
+      def rescue_500(error = nil)
         Rails.logger.error(
           "message: 500 InternalError #{request.url},
-          #{e&.message},
-          #{e&.class}"
+          #{error&.message},
+          #{error&.class}"
         )
         Raven.extra_context(params: params&.to_unsafe_h, url: request.url)
-        render json: { message: "internal error", status: 500 }
+        render json: { message: 'internal error', status: 500 }
       end
     end
   end
