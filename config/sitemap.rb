81197441fe94e3fe8b8e869d6248d1c2de0fbafa
hatsu <hajiwata0308@gmail.com>
2020-10-31 13:48:48 +0900

    紐付いているお店がないPathはsitemapに載せない

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -16,31 +16,40 @@ end
 require "csv"
 
 SitemapGenerator::Sitemap.create do
-  urls = CSV.read(Rails.root.join("config/datas", "404_list.csv"), headers: true).map { |data| data["URL"] }
+  add root_path, changefreq: 'daily', priority: 1.0
 
-  add root_path, changefreq: 'daily'
-  main_shop_eng_names = MainShop.all.pluck(:eng_name)
-  main_shop_eng_names.each do |main_shop_eng_name|
-    target_url = "/chain_shops/#{main_shop_eng_name}"
-    next if urls.include?(target_url)
-    add target_url, changefreq: "daily", priority: 1.0
+  main_shops = MainShop.all
+
+  main_shops.each do |main_shop|
+    next if main_shop.shops.blank?
+
+    target_url = "/chain_shops/#{main_shop.eng_name}"
+    add target_url, changefreq: "daily", priority: 0.8
   end
 
   Prefecture.all.each do |prefecture|
-    add "#{prefecture.name_e}", changefreq: "daily", priority: 1.0
-    main_shop_eng_names.each do |main_shop_eng_name|
-      target_url = "#{prefecture.name_e}/chain_shops/#{main_shop_eng_name}"
-      next if urls.include?(target_url)
+    next if prefecture.shops.blank?
+
+    add "#{prefecture.name_e}", changefreq: "daily", priority: 0.8
+
+    main_shops.each do |main_shop|
+      next if prefecture.shops.where(main_shop_id: main_shop.id).blank?
+
+      target_url = "#{prefecture.name_e}/chain_shops/#{main_shop.eng_name}"
       add target_url, changefreq: "daily", priority: 0.8
     end
   end
 
   City.all.select(:id, :code, :prefecture_id).each do |city|
-    add "#{city.prefecture_name_e}/#{city.code}", changefreq: "daily", priority: 0.8
-    main_shop_eng_names.each do |main_shop_eng_name|
-      target_url = "#{city.prefecture_name_e}/#{city.code}/chain_shops/#{main_shop_eng_name}"
-      next if urls.include?(target_url)
-      add target_url, changefreq: "daily", priority: 0.6
+    next if city.shops.blank?
+
+    add "#{city.prefecture_name_e}/#{city.code}", changefreq: "daily", priority: 0.6
+
+    main_shops.each do |main_shop|
+      next if city.shops.where(main_shop_id: main_shop.id).blank?
+
+      target_url = "#{city.prefecture_name_e}/#{city.code}/chain_shops/#{main_shop.eng_name}"
+      add target_url, changefreq: "daily", priority: 0.5
     end
   end
 
@@ -48,17 +57,18 @@ SitemapGenerator::Sitemap.create do
                           .select(:id, :prefecture_id, :city_id)
                           .preload(:city)
   stations.each do |station|
+    next if station.shops.blank?
+
     target_url = "#{station.prefecture_name_e}/#{station.city_code}/stations/#{station.id}"
-    next if urls.include?(target_url)
-    add target_url, changefreq: "daily", priority: 0.8
+    add target_url, changefreq: "daily", priority: 0.5
   end
 
   shops = Shop.where.not(city_id: nil)
                     .select(:id, :main_shop_id, :prefecture_id, :city_id)
                     .preload(:main_shop, :city)
   shops.all.each do |shop|
-    target_url = "#{shop.prefecture_name_e}/#{shop.city_code}/chain_shops/#{shop.main_shop_eng_name}/#{shop.id}"
-    next if urls.include?(target_url)
+    target_url = "#{shop.prefecture_name_e}/#{shop.city_code}/chain_shops/#{shop.main_shop.eng_name}/#{shop.id}"
+
     add target_url, changefreq: "daily", priority: 0.5
   end
 end
