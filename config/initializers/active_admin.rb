816cbadda10e5a4f3e78a6cfe77fd6fbbaa168cc
hatsu <hajiwata0308@gmail.com>
2021-03-14 15:57:45 +0900

    user webpacker activeadmin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -325,4 +325,6 @@ ActiveAdmin.setup do |config|
   # You can inherit it with own class and inject it for all resources
   #
   # config.order_clause = MyOrderClause
+
+  config.use_webpacker = true
 end
