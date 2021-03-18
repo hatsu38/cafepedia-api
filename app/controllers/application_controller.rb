e52e94c2416ede60a085e23ff09eac9a556a7d37
hatsu <hajiwata0308@gmail.com>
2021-03-19 00:10:02 +0900

    Fix by rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: Rails.application.credentials[:basic][:user_name],
                                password: Rails.application.credentials[:basic][:password],
-                               if: -> { request.path.start_with?("/admin") }
+                               if: -> { request.path.start_with?('/admin') }
 end
