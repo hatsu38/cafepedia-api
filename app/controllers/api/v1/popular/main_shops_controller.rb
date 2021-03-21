764b9c0c4c1a6507ad348349ae3a6118bb9ed034
hatsu <hajiwata0308@gmail.com>
2021-03-22 01:43:03 +0900

    Add Mainshops Db Cache

--- app/controllers/api/v1/popular/main_shops_controller.rb
+++ app/controllers/api/v1/popular/main_shops_controller.rb
@@ -4,7 +4,18 @@ module Api
       class MainShopsController < BaseController
         PER = 50
         def index
-          @main_shops = MainShop.popular.page(params[:page]).per(params[:per] || PER)
+          @main_shops = fetch_popular_main_shops
+        end
+
+        private
+        def fetch_popular_main_shops
+          Rails.cache.fetch("popular/main_shops/#{page_params}_#{per_params}_index_fetch_popular_main_shops", expires_in: 24.hours) do
+            MainShop.popular.page(page_params).per(per_params).to_a
+          end
+        end
+
+        def per_params
+          (params[:per] || PER).to_i
         end
       end
     end
